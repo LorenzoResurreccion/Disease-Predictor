cee1147aa5d4b567b8490da27fb4162c1be1b3ba
Lorenzo Resurreccion <lorenzo_rez@Lorenzos-MacBook-Air.local>
2025-11-16 21:16:11 -0600

    added api endpoints

--- APIs/Prediction.py
+++ APIs/Prediction.py
@@ -9,9 +9,12 @@ CORS(app)
 
 # list necessary features for for quick check and fill (median/mean/mode can be used)
 # maybe implement fill using user's health history and use general fill val if still missing
-features = {'Heart':{},
-            'Diabetes':{},
-            'CKD':{}}
+features = {'Heart':['age', 'sex', 'cp', 'trestbps',  'chol', 'fbs', 'restecg',
+                    'thalach', 'exang', 'oldpeak', 'slope', 'ca', 'thal'],
+            'Diabetes':['Age, Sex, HighChol, CholCheck, BMI, Smoker,  '],
+            'CKD':[]}
+
+fill_vals = {}
 
 # Path details
 ML_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'ML_Models')) 
@@ -21,15 +24,17 @@ Model_Files = {'Heart': 'heart_model.pkl',
 
 # Load the models once when the application starts
 Models = {} 
+def load_models(): 
+    for label, file in Model_Files.items():
+        path = os.path.join(ML_dir, file)
+        
+        #check if model exists, else assign None
+        try:
+            Models[label] = joblib.load(path)
+        except Exception as e:
+            Models[label] = None
 
-for label, file in Model_Files.items():
-    path = os.path.join(ML_dir, file)
-    
-    #check if model exists, else assign None
-    try:
-        Models[label] = joblib.load(path)
-    except Exception as e:
-        Models[label] = None
+load_models()
 
 
 """
@@ -41,24 +46,37 @@ data: json provided in request
 
 returns: risk of disease as float, else error if No prediction model is available for disease
 """
-def feature_pred(disease, data):
-    #check model exists
-    model = Models[disease]
-    if model is None:
-        return f"{disease} Prediction Model Temporarily Unavailable", 503
+def feature_pred(diseases, data):
+    Results = []
+
+    # iterate through models
+    for disease in diseases: 
+        try:
+            #check model exists
+            model = Models[disease]  
+            if model is None:
+                #model exists but is down
+                Results.append( {f'{disease}': "Prediction Model Temporarily Unavailable"})
     
-    #check features and fill
-    needed_features = features[disease]
-    for feature, fill_val in needed_features.items():
-        if feature not in data or data[feature] is None:
-            data[feature] = fill_val
-    
-    #predict
-    df = pd.DataFrame([data])
-    X = df[needed_features]
-    risk = model.predict_proba(X)[:,1][0]
-    
-    return  jsonify({'disease_risk': float(risk)})
+            #check features and fill
+            needed_features = features[disease]
+            for feature, fill_val in needed_features.items():
+                if feature not in data or data[feature] is None:
+                    data[feature] = fill_val
+            
+            #predict
+            df = pd.DataFrame([data])
+            X = df[needed_features]
+            risk = model.predict_proba(X)[:,1][0]
+
+            #append results
+            Results.append({f'{disease}': float(risk)})
+
+        except Exception as e:
+            #indicate model doesn't exist
+            Results.append( {f'{disease}': "No Exising Model"})
+
+    return jsonify(Results)
 
 
 """
@@ -80,22 +98,66 @@ def image_pred(disease, data):
 
 
 
-@app.route('/predict/<model_name>', methods=['POST'])
-def predict(model_name):
-    data = request.json
+@app.route('/prediction/<type>', methods=['POST'])
+def prediction(type):
+    req = request.json
+    diseases = req.get('diseases')
+    data = req.get('data')
+
 
     #call method based on model_name in path
-    match model_name:
-        case 'heart':
-            return feature_pred('Heart', data)
-        case  'diabetes':
-            return feature_pred('Diabetes', data)
-        case 'ckd':
-           return feature_pred('CKD', data)
-        case 'skin':
-            return image_pred('Skin', data)
+    match type:
+        case 'feature':
+            return feature_pred(diseases, data)
+        case 'image':
+            return image_pred(diseases, data)
         case _:
-            return "No Matching Model", 400
+            return "No Matching Models", 400
+        
+'''
+Accepts list of models to update models list and reload the models
+
+JSON should be in the structure:
+{model_name_1 : file_name_1, model_name_2 : file_name_2, ...}
+
+returns 200 indicating models were successfuly loaded and list updated
+'''
+@app.route('/models', methods=['POST'])
+def models():
+    data = request.json
+    Model_Files = data.get('models')
+    load_models()
+
+    return "Models Updated", 200
+
+
+'''
+Accepts list of fill values for disease risk based on features
+
+JSON should be in the structure:
+{feature_1 : val_1, feature_2 : val_2, ...}
+
+returns 200 indicating fill values were successfuly updated
+'''
+@app.route('/fill_values',  methods=['POST'])
+def fill_values():
+    data = request.json
+    fill_vals = data.get('values')
+
+    return "Fill Values updated", 200
+
+
+'''
+Accepts list of fill features for each model
+
+returns 200 indicating features were successfuly updated
+'''
+@app.route('/features',  methods=['POST'])
+def features():
+    data = request.json
+    features = data.get('features')
+
+    return "Features updated", 200
 
 if __name__ == '__main__':
     app.run(debug=True)
